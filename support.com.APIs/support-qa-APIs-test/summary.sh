33c490184c4baacec7e7a75467c37e0a43fd060b
Dipanjana Kundu <dkundu@lexmark.com>
2020-07-23 12:22:45 +0530

    Update summary.sh

--- support.com.APIs/support-qa-APIs-test/summary.sh
+++ support.com.APIs/support-qa-APIs-test/summary.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-file="SOLR-SoapUI/reports/html/overview-summary.html"
+file="support.com.APIs/support-qa-APIs-test/reports/html/overview-summary.html"  
 
 summary=$(grep "<td><a href=" $file | awk -F "</td><td>" '{printf "Total test summary.  Total:%s Error:%s Fail:%s Skip:%s", $2,$3,$4,$5}')
 
