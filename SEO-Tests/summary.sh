00b7148ad8e198c294bf872d10b3fc898842b326
Dipanjana Kundu <dkundu@lexmark.com>
2020-10-07 17:07:05 +0530

    Add files via upload

---
+++ SEO-Tests/summary.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+file="SEO-Tests/reports/html/overview-summary.html"
+
+summary=$(grep "<td><a href=" $file | awk -F "</td><td>" '{printf "Total test summary.  Total:%s Error:%s Fail:%s Skip:%s", $2,$3,$4,$5}')
+
+echo $summary
