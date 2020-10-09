46b3df603c92d3aa648525e1a5962532d633165c
Walter Francis <wally@theblackmoor.net>
2020-10-09 10:18:41 -0400

    Full path
    Forgot we start from the workspace root

---
+++ Fix/SEO-Tests/API-Tests/summary.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+path="SEO-Tests/API-Tests/results/"
+fail=`ls ${path}/*FAILED.txt | wc -l`
+total=`ls ${path}/*.txt | wc -l`
+error=0
+skip=0
+
+summary="Total test summary.  Total:${total} Error:${error} Fail:${fail} Skip:${skip}"
+
+echo $summary
