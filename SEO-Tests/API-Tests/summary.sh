7e1288cb22b02a93d22b00a775277637251d7eff
Dipanjana Kundu <dkundu@lexmark.com>
2020-10-09 14:17:00 +0530

    Add files via upload

---
+++ SEO-Tests/API-Tests/summary.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+file="SEO-Tests/API-Tests/reports/html/overview-summary.html"
+
+total=$(grep "<td><a href=" $file | awk -F "</td><td>" '{printf "%s", $2}')
+error=$(grep "<td><a href=" $file | awk -F "</td><td>" '{printf "%s", $3}')
+fail=$(grep "<td><a href=" $file | awk -F "</td><td>" '{printf "%s", $4}')
+skip=$(grep "<td><a href=" $file | awk -F "</td><td>" '{printf "%s", $5}')
+
+# What we did before, in case you need it again
+#summary=$(grep "<td><a href=" $file | awk -F "</td><td>" '{printf "Total test summary.  Total:%s Error:%s Fail:%s Skip:%s", $2,$3,$4,$5}')
+#echo $summary
+
+# Grab test steps from results output files, seems valid for this test anyway
+passed=`ls LPMD-SoapUI/results/*-OK.txt 2>/dev/null | wc -l`
+failed=`ls LPMD-SoapUI/results/*-FAILED.txt 2>/dev/null | wc -l`
+faketotal=`expr ${passed} + ${failed}`
+
+# New summary line using our faked total above as well as failed info above. 
+# Error and skipped still come from the HTML file
+echo "Total test summary. Total:${faketotal} Error:${error} Fail:${failed} Skip:${skip}"
